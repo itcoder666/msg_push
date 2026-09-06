61f78a3e8b9a6fbba1d3e92c16a989f83c088c76
itzhss <itzhss@outlook.com>
2026-09-06 23:11:31 +0800

    增加 topic 打印

--- src/msg_push/pushplus.py
+++ src/msg_push/pushplus.py
@@ -34,7 +34,7 @@ def build_pushplus_payload(
     if topic:
         payload["topic"] = topic
         print(f"pushplus topic: {topic}")
-
+    print(f"pushplus topic: {topic}")
     return payload
 
 
